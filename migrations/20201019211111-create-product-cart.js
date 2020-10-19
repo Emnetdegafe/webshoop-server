948d4d5c1c283b77da7d4b74b9029c1659588105
Emnetdegafe <emnetdegafe@gmail.com>
2020-10-19 23:17:17 +0200

    cart, productCart, order models setup + relations

---
+++ migrations/20201019211111-create-product-cart.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('productCarts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      totalPrice: {
+        type: Sequelize.FLOAT
+      },
+      totalQty: {
+        type: Sequelize.FLOAT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('productCarts');
+  }
+};
